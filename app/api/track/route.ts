5f8810bfefec7df34cadad4ce9890d6d21b6cc3c
Tyson Hichman <tysonhichman@gmail.com>
2025-07-14 12:27:56 -0500

    fixing content load

--- app/api/track/route.ts
+++ app/api/track/route.ts
@@ -4,6 +4,8 @@ import supabase from '@/lib/supabase/server';
 export async function POST(req: NextRequest) {
     const body = await req.json();
 
+    console.log('Received tracking data:', body);
+
     const ip =
         req.headers.get('x-forwarded-for') ||
         req.headers.get('x-real-ip') ||
